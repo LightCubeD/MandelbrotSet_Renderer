562e2411afe3f48145f123e8a814bc36daf69dc3
LightCube <dev.lightcube@gmail.com>
2019-03-26 16:25:59 +0100

    made it possible to do multithreading with an offset

--- MandelRenderer/main.cpp
+++ MandelRenderer/main.cpp
@@ -8,8 +8,15 @@ int main(int argc, char** argv) {
 			system(("cmd.exe /c start renderer.exe " + std::to_string(i) + " " + argv[1]).c_str());
 		}
 	}
+	if (argc == 3) {
+		unsigned int offset = std::stoi(argv[2]);
+		unsigned int threads = std::stoi(argv[1]);
+		for (int i = 0; i < threads; i++) {
+			system(("cmd.exe /c start renderer.exe " + std::to_string(offset + i) + " " + argv[1]).c_str());
+		}
+	}
 	else {
-		std::cout << "Syntax: MandelRenderer.exe [threads]" << std::endl;
+		std::cout << "Syntax: MandelRenderer.exe [threads] ([offset] = 0)" << std::endl;
 	}
 	return 0;
 }
